5946562e4b45415464ee8159beecf92d16b9a22f
Olguín Morales César Armando <olgcesar@gmail.com>
2018-10-03 11:37:48 -0500

    Servicios Rest

--- src/providers/user-service/user-service.ts
+++ src/providers/user-service/user-service.ts
@@ -11,7 +11,143 @@ import { Injectable } from '@angular/core';
 export class UserServiceProvider {
 
   constructor(public http: HttpClient) {
-    console.log('Hello UserServiceProvider Provider');
+    //console.log('Hello UserServiceProvider Provider');
+  }
+
+  baseUrl = "http://solucionesgp.com/autopartes";
+
+  getUsers() {
+    return this.http.get('https://randomuser.me/api/?results=25');
+  }
+  
+  getArticulos() {
+    return this.http.get(this.baseUrl + '/articulo/');
+  }
+
+  getMarcas() {
+    return this.http.get(this.baseUrl + '/marcas/');
+  }
+
+  getModelo(id) {
+    return new Promise(resolve => {
+      this.http.get(this.baseUrl + '/modelos/' + id).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getArticuloById(id) {
+    return new Promise(resolve => {
+      this.http.get(this.baseUrl + '/articulo/' + id).subscribe(data => {
+        resolve(data)
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+  getArticuloByUser(id) {
+    return new Promise(resolve => {
+      this.http.get(this.baseUrl + '/articulo/usuario/' + id).subscribe(data => {
+        resolve(data)
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getComentarios(id) {
+    return new Promise(resolve => {
+      this.http.get(this.baseUrl + '/articulo/' + id + '/comentario/').subscribe(data => {
+        resolve(data)
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  postComentario(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.baseUrl + '/articulo/comentario/', data)
+        .subscribe(res => {
+          resolve(res);
+          console.log(data);
+        }, err => {
+          console.log(err);
+        });
+    });
+  }
+
+  postArticulo(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.baseUrl + '/articulo/', data)
+        .subscribe(res => {
+          resolve(res);
+          console.log(data);
+        }, err => {
+          console.log(err);
+        });
+    });
+  }
+
+  postFoto(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.baseUrl + '/articulo/fotos/', data)
+        .subscribe(res => {
+          resolve(res);
+        }, err => {
+          console.log(err);
+        });
+    });
+  }
+
+  postRegistro(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.baseUrl + '/registro/', data)
+        .subscribe(res => {
+          resolve(res);
+          console.log(data);
+        }, err => {
+          console.log(err);
+        });
+    });
+  }
+
+  getLoggin(email, password) {
+    return new Promise(resolve => {
+      this.http.get(this.baseUrl + '/registro/' + email + '/' + password).subscribe(data => {
+        resolve(data)
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  checkEmail(email){
+    return new Promise(resolve => {
+      this.http.get(this.baseUrl + '/registro/' + email).subscribe(data => {
+        resolve(data)
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  postPedido(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.baseUrl + '/pedido/', data)
+        .subscribe(res => {
+          resolve(res);
+          console.log(data);
+        }, err => {
+          console.log(err);
+        });
+    });
+  }
+
+  getPedidos() {
+    return this.http.get(this.baseUrl + '/pedido/');
   }
 
 }
