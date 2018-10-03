09a90a26ed15ea5a2c1bf33cb613c6551c77d143
Olguín Morales César Armando <olgcesar@gmail.com>
2018-10-03 12:28:20 -0500

    revisa si existe un login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Events } from 'ionic-angular';
+import { UserServiceProvider } from '../../providers/user-service/user-service';
+import { HacerPedidoPage } from '../hacer-pedido/hacer-pedido';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  selectedItem: any;
+  pedidos;
+  items;
+  displayedImages;
+  
+  constructor(public navCtrl: NavController, public restService: UserServiceProvider, public events: Events) {
 
   }
 
+  ionViewDidLoad() {
+
+    if (window.localStorage.getItem("user") != null && window.localStorage.getItem("pass") != null) {
+      //Hay una sesión iniciada
+      //Dirige a la pantalla principal ya logueada.
+      this.events.publish('user:loggedin');
+    } else {
+      //Manda la pantalla de inicio de sesión o autentificación
+      this.events.publish('user:loggedout');
+    }
+    this.load();
+  }
+  
+  ionViewCanEnter() {
+    window.location.reload;
+    this.load();
+  }
+
+  ionViewWillEnter() {
+    window.location.reload;
+    this.load();
+  }
+
+  pushPage() {
+    this.navCtrl.push(HacerPedidoPage);
+  }
+
+  load() {
+    this.restService.getPedidos().subscribe(
+      data => {
+        // Success
+        this.pedidos = data;
+        //this.items = data;
+        // console.log(data);
+        this.items = data;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
 }
