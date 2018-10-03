fdc1c44836fbe3f8fa6292c1d718def40d517e9e
Olguín Morales César Armando <olgcesar@gmail.com>
2018-10-03 11:30:34 -0500

    app module  paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,26 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CuentaPage } from '../pages/cuenta/cuenta';
+import { FavoritosPage } from '../pages/favoritos/favoritos';
+import { HacerPedidoPage } from '../pages/hacer-pedido/hacer-pedido';
+import { LoginPage } from '../pages/login/login';
+import { MiPedidoPage } from '../pages/mi-pedido/mi-pedido';
+import { MiVentaPage } from '../pages/mi-venta/mi-venta';
+import { MisPedidosPage } from '../pages/mis-pedidos/mis-pedidos';
+import { MisVentasPage } from '../pages/mis-ventas/mis-ventas';
+import { ModificarPedidoPage } from '../pages/modificar-pedido/modificar-pedido';
+import { ModificarVentaPage } from '../pages/modificar-venta/modificar-venta';
+import { PedidoPage } from '../pages/pedido/pedido';
+import { PedidosPage } from '../pages/pedidos/pedidos';
+import { PreguntaPage } from '../pages/pregunta/pregunta';
+import { PreguntasPage } from '../pages/preguntas/preguntas';
+import { RealizarVentaPage } from '../pages/realizar-venta/realizar-venta';
+import { RegistrarsePage } from '../pages/registrarse/registrarse';
+import { ResponderPage } from '../pages/responder/responder';
+import { UsuarioPage } from '../pages/usuario/usuario';
+import { VentaPage } from '../pages/venta/venta';
+import { VentasPage } from '../pages/ventas/ventas';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -11,7 +31,28 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CuentaPage,
+    FavoritosPage,
+    HacerPedidoPage,
+    LoginPage,
+    MiPedidoPage,
+    MiVentaPage,
+    MisPedidosPage,
+    MisVentasPage,
+    ModificarPedidoPage,
+    ModificarVentaPage,
+    PedidoPage,
+    PedidosPage,
+    PreguntaPage,
+    PreguntasPage,
+    RealizarVentaPage,
+    RegistrarsePage,
+    ResponderPage,
+    UsuarioPage,
+    VentaPage,
+    VentasPage
+
   ],
   imports: [
     BrowserModule,
@@ -20,12 +61,32 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CuentaPage,
+    FavoritosPage,
+    HacerPedidoPage,
+    LoginPage,
+    MiPedidoPage,
+    MiVentaPage,
+    MisPedidosPage,
+    MisVentasPage,
+    ModificarPedidoPage,
+    ModificarVentaPage,
+    PedidoPage,
+    PedidosPage,
+    PreguntaPage,
+    PreguntasPage,
+    RealizarVentaPage,
+    RegistrarsePage,
+    ResponderPage,
+    UsuarioPage,
+    VentaPage,
+    VentasPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
