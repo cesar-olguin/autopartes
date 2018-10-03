ad3350c67e9e2beb28104a3a638f0957ff28d434
Olguín Morales César Armando <olgcesar@gmail.com>
2018-10-03 12:28:01 -0500

    realiza el login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, AlertController, App, Events } from 'ionic-angular';
+import { UserServiceProvider } from './../../providers/user-service/user-service';
+import { empty } from 'rxjs/Observer';
+import { Storage } from '@ionic/storage';
+import { Md5 } from 'ts-md5';
+import { HomePage } from '../home/home';
 /**
  * Generated class for the LoginPage page.
  *
@@ -15,11 +19,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public restService: UserServiceProvider, public alertCtrl: AlertController, private appCtrl: App, private storage: Storage, public events: Events) {
   }
 
+  public Usuario;
+  public Password;
+
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    //this.Usuario = this.getFromStorageAsyncUser();
+    //this.Password = this.getFromStorageAsyncPass();
+  }
+
+  login() {
+    if (this.Usuario == empty || this.Password == empty) {
+      this.sinDatos();
+    }
+    else {
+      this.restService.getLoggin(this.Usuario, Md5.hashStr(this.Password)).then(data => {
+        console.log(JSON.stringify(data));
+        if ((JSON.stringify(data)) == "[]") {
+          this.correoError();
+        }
+        else {
+          this.storage.set('user', this.Usuario);
+          this.storage.set('pass', this.Password);
+
+          window.localStorage.setItem('user',this.Usuario);
+          window.localStorage.setItem('pass',this.Password);
+
+          this.events.publish('user:loggedin');
+          this.appCtrl.getRootNav().setRoot(HomePage);
+        }
+      });
+    }
+  }
+
+  sinDatos() {
+    let alert = this.alertCtrl.create({
+      title: 'ERROR',
+      subTitle: 'Llena los campos.',
+      buttons: ['Aceptar']
+    });
+    alert.present();
+  }
+
+  correoError() {
+    let alert = this.alertCtrl.create({
+      title: 'ERROR',
+      subTitle: 'Error en el usuario y/o contraseña.',
+      buttons: ['Aceptar']
+    });
+    alert.present();
   }
 
 }
